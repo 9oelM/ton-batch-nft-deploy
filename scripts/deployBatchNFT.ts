49b93ccd1eaebc598dc5039fa95c7eba127df678
9oelM <hj923@hotmail.com>
2025-01-23 11:58:58 +0900

    feat: build collection and item

--- scripts/deployBatchNFT.ts
+++ scripts/deployBatchNFT.ts
@@ -1,13 +1,12 @@
 import { toNano } from '@ton/core';
-import { BatchNFT } from '../wrappers/BatchNFT';
 import { compile, NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const batchNFT = provider.open(BatchNFT.createFromConfig({}, await compile('BatchNFT')));
+    // const batchNFT = provider.open(BatchNFT.createFromConfig({}, await compile('BatchNFT')));
 
-    await batchNFT.sendDeploy(provider.sender(), toNano('0.05'));
+    // await batchNFT.sendDeploy(provider.sender(), toNano('0.05'));
 
-    await provider.waitForDeploy(batchNFT.address);
+    // await provider.waitForDeploy(batchNFT.address);
 
     // run methods on `batchNFT`
 }
